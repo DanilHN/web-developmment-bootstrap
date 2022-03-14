da74579d557d8490386fb2969db575d0ac2bf624
DanilHN <95386266+DanilHN@users.noreply.github.com>
2022-03-14 20:00:53 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -17,9 +17,36 @@ $(document).ready(function() {
         slidesToScroll: 3,
         arrows: false,
         dots: true,
-        dotsClass: 'dots-style'
-    });
+        dotsClass: 'dots-style',
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
 
+            }
+        }]
+    });
+    $('ul.menu a[href^="#"').click(function() {
+        var target = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 500);
+        $('ul.menu a[href^="#"').css({ 'color': '#212121' });
+        $(this).css({ 'color': '#004bee' });
+        return false;
+    });
+    $(window).scroll(function() {
+        if ($(this).scrollTop() != 0)
+            $('#toTop').fadeIn();
+        else
+            $('#toTop').fadeOut();
+    });
+    $('#toTop').click(function() {
+        $('body, html').animate({
+            scrollTop: 0
+        }, 800);
+    })
 });
 
 /* 
